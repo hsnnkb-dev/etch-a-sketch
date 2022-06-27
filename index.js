57c5ec3656523f93bc4f00193560bde348a821df
Hasan N <hsnnkb.dev@gmail.com>
2022-06-27 12:36:00 +0100

    Iteration 3 - Colouring functionality
    Add ability to colur grid squares black to index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const mainDiv = document.getElementById("container");
-console.log(mainDiv);
+//console.log(mainDiv);
 
 for (let i=0; i<=15; i++) {
     let div = document.createElement("div");
-    console.log(i);
+    //console.log(i);
     mainDiv.appendChild(div);
 }
+
+function changeColor(e) {
+    //console.log(e);
+    e.target.classList.add('black');
+
+}
+
+let divs = mainDiv.querySelectorAll("div");
+console.log(divs);
+
+divs.forEach(div => {
+    div.addEventListener("mouseover", changeColor)
+});
