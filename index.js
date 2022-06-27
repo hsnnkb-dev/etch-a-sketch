c50b299598a20c9023bf5c73698a938707f93cff
Hasan N <hsnnkb.dev@gmail.com>
2022-06-27 14:19:52 +0100

    Iteration 4 - User Input to create grid
    Add button to get user input to index.html
    Add functionality to index.js to create grid with the size matching the
    user input

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const mainDiv = document.getElementById("container");
+let divs = "";
 //console.log(mainDiv);
-
-for (let i=0; i<=15; i++) {
-    let div = document.createElement("div");
-    //console.log(i);
-    mainDiv.appendChild(div);
-}
 
 function changeColor(e) {
     //console.log(e);
@@ -13,9 +8,45 @@ function changeColor(e) {
 
 }
 
-let divs = mainDiv.querySelectorAll("div");
-console.log(divs);
+function addColour() {
+    divs = mainDiv.querySelectorAll("div");
+    divs.forEach(div => {
+        div.addEventListener("mouseover", changeColor)
+    });
+}
 
-divs.forEach(div => {
-    div.addEventListener("mouseover", changeColor)
-});
+
+function createGrid(number) {
+    let newWidth = (number * 102);
+    mainDiv.style.width = newWidth + "px";
+
+    for (let i=0; i<number; i++) {
+        for (let j=0; j<number; j++) {
+            let div = document.createElement("div");
+        //console.log(i);
+        mainDiv.appendChild(div);
+        }
+    }
+    addColour();
+}
+
+function resetGrid() {
+    while (mainDiv.firstChild) {
+        mainDiv.removeChild(mainDiv.firstChild);
+    }
+}
+
+function setGrid() {
+    let userInput = window.prompt("Enter a number: e.g. '4' creates a 4*4 grid with 16 squares. Max size = 10", 4);
+    if (userInput > 10) {userInput = 10};
+    if (userInput < 1) {userInput = 1};
+    resetGrid();
+    createGrid(userInput);
+}
+
+function initialGrid(number) {
+    createGrid(number);
+    addColour();
+}
+
+initialGrid(4);
